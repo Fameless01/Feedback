5a59cb2c95d28aab847b8fe10afcaea4b3731cda
Fameless01 <22bca0259@gmail.com>
2024-03-21 19:39:05 +0530

    ver 0.2

--- Emotion_Detection.py
+++ Emotion_Detection.py
@@ -1,56 +1,22 @@
+
 import cv2
-from fer import FER
-
-# Initialize the FER model (optional: set mtcnn=True for MTCNN face detection)
-detector = FER(mtcnn=True)
-
-# Load the image or capture video from webcam
-# For image:
-# img = cv2.imread("path/to/your/image.jpg")  # Replace with your image path
-
-# For webcam:
-cap = cv2.VideoCapture(0)
-
-while True:
-    # Read image from webcam (for video)
-    ret, frame = cap.read()
-
-    # Detect emotions
-    emotions = detector.detect_emotions(frame)
-
-    # Draw rectangle around detected face(s) (optional)
-    if emotions:
-        for emotion in emotions:
-            box = emotion["box"]
-            cv2.rectangle(frame, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0, 255, 0), 2)
-
-    # Get the dominant emotion and its score
-    if emotions:
-        dominant_emotion, score = detector.top_emotion(emotions)
-        print("Dominant Emotion:", dominant_emotion, "Score:", score)
-
-    # Display the frame with detected emotions (optional)
-    cv2.imshow("Facial Expression Detection", frame)
-
-    # Exit on 'q' press
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-
-# Release resources (for webcam)
+import os
+from deepface import DeepFace
+face_classifier =cv2.CascadeClassifier()
+face_classifier.load(cv2.samples.findFile("/home/fameless/my_venv/lib/python3.11/site-packages/cv2/data/haarcascade_frontalface_default.xml"))
+cap=cv2.VideoCapture(0)
+while(True):
+  ret, frame =cap.read()
+  frame_gray =cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  faces =face_classifier.detectMultiScale(frame_gray)
+  response =DeepFace.analyze(frame, actions=("emotion"), enforce_detection=False)
+#   print("\n\nRsult",response, end='')
+  for face in faces:
+    x, y, w, h = face
+    cv2.putText(frame, text=response[0].get('dominant_emotion'), org=(x, y), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1.0 , color=(0,255,0))
+    new_frame =cv2.rectangle(frame, (x, y), (x+w,y+h), color=(255, 0, 0), thickness= 2)
+    cv2.imshow("xx", new_frame)
+  if cv2.waitKey(1)& 0xFF == ord("q"):
+    break
 cap.release()
 cv2.destroyAllWindows()
-
-
-
-# import cv2
-# cap = cv2.VideoCapture(0) 
-
-# while True:
-#     ret,frame = cap.read()
-#     cv2.imshow("title",frame)
-
-#     if cv2.waitKey(1) == ord('q'):
-#       break
-
-# cap.release()
-# cv2.destroyAllWindows()
